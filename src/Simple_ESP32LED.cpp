775e40ecd037a865d597bedddf3a38444746a333
grunnels <3443144+grunnels@users.noreply.github.com>
2025-04-17 19:59:10 -0500

    Added blink(int duration)

--- src/Simple_ESP32LED.cpp
+++ src/Simple_ESP32LED.cpp
@@ -12,91 +12,112 @@
  *
  ******************************************************************************/
 
-#define LED_BUILD_TYPE_4 4 // idf_ver prior to 5.x
-#define LED_BUILD_TYPE_5 5 // idf_ver post 5.x
-
-#ifndef LED_BUILD_TYPE
-#error Must define LED_BUILD_TYPE...  Check IDF_VER
-#endif
-
-#include "Simple_ESP32LED.h"
-
-#if LED_BUILD_TYPE == LED_BUILD_TYPE_4
-const int BLINK_VALUE = 100;
-const int RESOLUTION = 8;
-#ifdef LED_ON_WHEN_HIGH
-const int LED_ON_VALUE = 255;
-const int LED_OFF_VALUE = 0;
-#else
-const int LED_ON_VALUE = 0;
-const int LED_OFF_VALUE = 255;
-#endif
-#endif
-#if LED_BUILD_TYPE == LED_BUILD_TYPE_5
-const int BLINK_VALUE = 8000;
-const int RESOLUTION = 14;
-#ifdef LED_ON_WHEN_HIGH
-const int LED_ON_VALUE = 0xFFFF;
-const int LED_OFF_VALUE = 0;
-#else
-const int LED_ON_VALUE = 0;
-const int LED_OFF_VALUE = 0xFFFF;
-#endif
-#endif
-
-int rootChannel = 0;
-
-void SimpleESP32_LED::init(uint8_t lp, uint16_t blinkRate)
-{
-    channel = rootChannel;
-    ledPin = lp;
-    rootChannel++;
-    rootChannel++;
-    // IF (IDF_VER MATCHES "v4.") can I use this in the future??
-
-#if LED_BUILD_TYPE == LED_BUILD_TYPE_5
-    ledcAttachChannel(ledPin, blinkRate, RESOLUTION, channel);
-#endif
-#if LED_BUILD_TYPE == LED_BUILD_TYPE_4
-    ledcSetup(channel, blinkRate, RESOLUTION);
-    ledcAttachPin(ledPin, channel);
-#endif
-}
-
-void SimpleESP32_LED::turnLEDOn(int ledStatus)
-{
-    int blinkVal = 0;
-    switch (ledStatus)
-    {
-    case LED_ON:
-        blinkVal = 0;
-        break;
-    case LED_OFF:
-        blinkVal = LED_OFF_VALUE;
-        break;
-    case LED_BLINK:
-        blinkVal = BLINK_VALUE;
-        break;
-    }
-#if LED_BUILD_TYPE == LED_BUILD_TYPE_5
-    ledcWrite(ledPin, blinkVal);
-#endif
-#if LED_BUILD_TYPE == LED_BUILD_TYPE_4
-    ledcWrite(channel, blinkVal);
-#endif
-}
-
-void SimpleESP32_LED::turnLEDOn()
-{
-    turnLEDOn(LED_ON);
-}
-
-void SimpleESP32_LED::turnLEDOff()
-{
-    turnLEDOn(LED_OFF);
-}
-
-void SimpleESP32_LED::blink()
-{
-    turnLEDOn(LED_BLINK);
-}
+ #define LED_BUILD_TYPE_4 4 // idf_ver prior to 5.x
+ #define LED_BUILD_TYPE_5 5 // idf_ver post 5.x
+ 
+ #ifndef LED_BUILD_TYPE
+ #error Must define LED_BUILD_TYPE...  Check IDF_VER
+ #endif
+ 
+ #include "Simple_ESP32LED.h"
+ 
+ #if LED_BUILD_TYPE == LED_BUILD_TYPE_4
+ const int BLINK_VALUE = 100;
+ const int RESOLUTION = 8;
+ #ifdef LED_ON_WHEN_HIGH
+ const int LED_ON_VALUE = 255;
+ const int LED_OFF_VALUE = 0;
+ #else
+ const int LED_ON_VALUE = 0;
+ const int LED_OFF_VALUE = 255;
+ #endif
+ #endif
+ #if LED_BUILD_TYPE == LED_BUILD_TYPE_5
+ const int BLINK_VALUE = 8000;
+ const int RESOLUTION = 14;
+ #ifdef LED_ON_WHEN_HIGH
+ const int LED_ON_VALUE = 0xFFFF;
+ const int LED_OFF_VALUE = 0;
+ #else
+ const int LED_ON_VALUE = 0;
+ const int LED_OFF_VALUE = 0xFFFF;
+ #endif
+ #endif
+ 
+ int rootChannel = 0;
+ 
+ void SimpleESP32_LED::init(uint8_t lp, uint16_t blinkRate)
+ {
+     channel = rootChannel;
+     ledPin = lp;
+     rootChannel++;
+     rootChannel++;
+     // IF (IDF_VER MATCHES "v4.") can I use this in the future??
+ 
+ #if LED_BUILD_TYPE == LED_BUILD_TYPE_5
+     ledcAttachChannel(ledPin, blinkRate, RESOLUTION, channel);
+ #endif
+ #if LED_BUILD_TYPE == LED_BUILD_TYPE_4
+     ledcSetup(channel, blinkRate, RESOLUTION);
+     ledcAttachPin(ledPin, channel);
+ #endif
+ }
+ 
+ void SimpleESP32_LED::turnLEDOn(int ledStatus)
+ {
+     int blinkVal = 0;
+     switch (ledStatus)
+     {
+     case LED_ON:
+         blinkVal = LED_ON_VALUE;
+         break;
+     case LED_OFF:
+         blinkVal = LED_OFF_VALUE;
+         break;
+     case LED_BLINK:
+         blinkVal = BLINK_VALUE;
+         break;
+     }
+ #if LED_BUILD_TYPE == LED_BUILD_TYPE_5
+     ledcWrite(ledPin, blinkVal);
+ #endif
+ #if LED_BUILD_TYPE == LED_BUILD_TYPE_4
+     ledcWrite(channel, blinkVal);
+ #endif
+ }
+ 
+ void SimpleESP32_LED::turnLEDOn()
+ {
+     turnLEDOn(LED_ON);
+ }
+ 
+ void SimpleESP32_LED::turnLEDOff()
+ {
+     turnLEDOn(LED_OFF);
+ }
+ 
+ // Turn off LED when timer expires
+ void autoBlinkCallback(TimerHandle_t xTimer)
+ {
+     SimpleESP32_LED *led = static_cast<SimpleESP32_LED *>(pvTimerGetTimerID(xTimer)); // Retrieve the pointer to class
+     assert(led);                                                                      // Sanity check
+     led->turnLEDOff();                                                                // Forward to the real callback
+ }
+ 
+ void SimpleESP32_LED::blink(int duration)
+ {
+     turnLEDOn(LED_BLINK);
+ 
+     if (duration != 0)
+     {
+         blickOffTimer = xTimerCreate(
+             "ledOff",                      // Name of timer
+             duration / portTICK_PERIOD_MS, // Period of timer (in ticks)
+             pdFALSE,                       // Auto-reload
+             static_cast<void *>(this),     // Send a pointer to this object
+             autoBlinkCallback);            // Callback function
+ 
+         xTimerStart(blickOffTimer, portMAX_DELAY);
+     }
+ }
+ 
--- src/Simple_ESP32LED.h
+++ src/Simple_ESP32LED.h
@@ -12,39 +12,42 @@
  *
  ******************************************************************************/
 
-#ifndef SIMPLE_ESP32LED_h
-#define SIMPLE_ESP32LED_h
-
-#include <Arduino.h>
-
-
-typedef enum ESP32LED_MODE
-{
-    LED_OFF = 0,
-    LED_ON = 1,
-    LED_BLINK = 2
-} esp32ledMode;
-
-typedef enum ESP32LED_BLINK
-{
-    LED_SLOW = 4,
-    LED_MEDIUM = 8,
-    LED_FAST = 16
-} esp32ledBlink;
-
-class SimpleESP32_LED
-{
-public:
-    void init(uint8_t lp, uint16_t blinkRate = LED_MEDIUM);
-    void turnLEDOn(int ledStatus);
-    void turnLEDOn();
-    void turnLEDOff();
-    void blink();
-
-private:
-    uint8_t ledPin;
-    uint16_t frequency;
-    uint16_t channel;
-};
-
-#endif
+ #ifndef SIMPLE_ESP32LED_h
+ #define SIMPLE_ESP32LED_h
+ 
+ #include <Arduino.h>
+ 
+ 
+ typedef enum ESP32LED_MODE
+ {
+     LED_OFF = 0,
+     LED_ON = 1,
+     LED_BLINK = 2
+ } esp32ledMode;
+ 
+ typedef enum ESP32LED_BLINK
+ {
+     LED_SLOW = 4,
+     LED_MEDIUM = 8,
+     LED_FAST = 16
+ } esp32ledBlink;
+ 
+ class SimpleESP32_LED
+ {
+ public:
+     void init(uint8_t lp, uint16_t blinkRate = LED_MEDIUM);
+     void turnLEDOn(int ledStatus);
+     void turnLEDOn();
+     void turnLEDOff();
+     void blink(int duration = 0);
+ 
+ private:
+     uint8_t ledPin;
+     uint16_t frequency;
+     uint16_t channel;
+     TimerHandle_t blickOffTimer = NULL;
+ 
+ };
+ 
+ #endif
+ 
